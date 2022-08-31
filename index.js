5f25f86cd5876fb3f119cde7d90857a12881e0d2
Yingyang Zhang <yingyang.zhang95@gmail.com>
2022-08-30 20:17:09 -0400

    commit tree 9a2bd999e98599a22e017ca13c0b65c36b3bf6dc

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const newCats = [...cats];
+    newCats.push('Broom');
+    return newCats;
+}
+
+function prependCat(){
+    const newCats = [...cats];
+    newCats.unshift('Arnold');
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = [...cats].slice(0, 2);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = cats.slice(1);
+    return newCats;
+}
